a872aef83f1a6ebf3eb6d059c130ac4d2c15e3e9
Aviad Blumenfeld <aviadblu@gmail.com>
2021-06-24 16:50:50 +0300

    Initial commit

---
+++ RedisLoggerExample/RedisLogger.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisLoggerExample
+{
+    public static class RedisLogger
+    {
+        private static ConnectionMultiplexer _redisClient;
+        private const string RedisHost = "localhost";
+        private const int RedisPort = 6379;
+
+        public static void Init()
+        {
+            _redisClient = ConnectionMultiplexer.Connect($"{RedisHost}:{RedisPort},allowAdmin=true");
+        }
+
+        private static readonly Random Random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        public static async Task WriteLogMessage(int id, string msg)
+        {
+            await Task.Run(() =>
+            {
+                long ticks = DateTime.Now.Ticks;
+                var key = $"pddrlog_{ticks}_{RandomString(5)}_{id}";
+                var db = _redisClient.GetDatabase();
+                db.StringSet(key, msg);
+            });
+        }
+
+        public static List<KeyValuePair<string, string>> GetAllLogs(int id = -1)
+        {
+            var server = _redisClient.GetServer(RedisHost, RedisPort);
+            var db = _redisClient.GetDatabase();
+            IEnumerable<RedisKey> keys;
+            if (id == -1)
+            {
+                keys = server.Keys();
+            }
+            else
+            {
+                keys = server.Keys(pattern: $"*_{id}");
+            }
+
+            string[] keysArr = keys.Select(key => (string) key).ToArray();
+            var results = new List<KeyValuePair<string, string>>();
+            foreach (string key in keysArr)
+            {
+                results.Add(new KeyValuePair<string, string>(key, db.StringGet(key)));
+            }
+
+            return results.OrderBy(x => x.Key).ToList();
+        }
+
+        public static void DeleteLogByActionId(int id)
+        {
+            var server = _redisClient.GetServer("localhost", 6379);
+            var db = _redisClient.GetDatabase();
+            var keys = server.Keys(pattern: $"*_{id}");
+            string[] keysArr = keys.Select(key => (string) key).ToArray();
+            foreach (string key in keysArr)
+            {
+                db.KeyDelete(key);
+            }
+        }
+
+        public static void Dispose()
+        {
+            _redisClient.Dispose();
+        }
+    }
+}
---
+++ RedisLoggerExample/RedisLoggerTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Xunit;
+using StackExchange.Redis;
+using Xunit.Abstractions;
+
+namespace RedisLoggerExample
+{
+    public class Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Tests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            using (var redis = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var db = redis.GetDatabase();
+                db.StringSet("mykey", "some value");
+                var value = db.StringGet("mykey");
+                Assert.Equal(value, "some value");
+            }
+        }
+
+        [Fact]
+        public async void LogWrite()
+        {
+            RedisLogger.Init();
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < 10; i++)
+            {
+                await RedisLogger.WriteLogMessage(54, $"[{i}] my special message");
+            }
+
+            stopwatch.Stop();
+            _testOutputHelper.WriteLine(stopwatch.ElapsedMilliseconds.ToString());
+        }
+
+        [Fact]
+        public void GetAllKeysById_WriteToFile()
+        {
+            RedisLogger.Init();
+            var key = 54;
+            var logs = RedisLogger.GetAllLogs(key);
+            using (var writetext = new StreamWriter("log_54.txt"))
+            {
+                logs.ForEach(log =>
+                {
+                    var keyparts = log.Key.Split(new[] {"_"}, StringSplitOptions.None);
+                    writetext.WriteLine($"[{new DateTime(long.Parse(keyparts[1]))}][{keyparts[3]}] {log.Value}");
+                });
+            }
+        }
+
+        [Fact]
+        public void WriteAllLogsToFile()
+        {
+            RedisLogger.Init();
+            var logs = RedisLogger.GetAllLogs();
+            using (var writetext = new StreamWriter("all_logs_output.txt"))
+            {
+                logs.ForEach(log =>
+                {
+                    var keyparts = log.Key.Split(new[] {"_"}, StringSplitOptions.None);
+                    writetext.WriteLine($"[{new DateTime(long.Parse(keyparts[1]))}][{keyparts[3]}] {log.Value}");
+                });
+            }
+        }
+
+        [Fact]
+        public void DeleteAllKeysById()
+        {
+            RedisLogger.Init();
+            var key = 54;
+            RedisLogger.DeleteLogByActionId(key);
+        }
+    }
+}
